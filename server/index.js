67f548286251e8e0d877bf7f41886d901f40b1d6
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-10-25 19:15:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,10 +15,6 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-// const CONNECTION_URL =
-// 	'mongodb+srv://officerAbhi:officerabhinavsstclub@cluster0.wjpksth.mongodb.net/';
-
-// const PORT = process.env.PORT || 5000;
 const PORT = process.env.PORT || 5000;
 
 mongoose
